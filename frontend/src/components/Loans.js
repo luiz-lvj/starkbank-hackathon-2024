2fc71ab48025b7204334d8436b667ce78a95d9ed
Luiz <lvj253@gmail.com>
2024-08-31 18:17:02 -0300

    ïnitial setup

---
+++ frontend/src/components/Loans.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+
+export default function Loans() {
+    return (
+        <LoansStyle>
+            <h1>Componente de empréstimo aqui</h1>
+        </LoansStyle>
+    );
+}
+
+const LoansStyle = styled.div`
+    
+`; //css aqui
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import Loans from "../components/Loans";
+
+
+
+export default function Home() {
+    return (
+        <HomeStyle>
+            Codar aqui
+            <Loans />
+        </HomeStyle>
+    );
+}
+
+
+const HomeStyle = styled.div`
+
+`; //css aqui

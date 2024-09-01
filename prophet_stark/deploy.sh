c26f55ffbf012e6803d6bfc1979f41d91f27e510
andredifs <andrefirsan@hotmail.com>
2024-09-01 12:30:48 -0300

    feat: add profhet api on cloud run

---
+++ prophet_stark/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Configurações iniciais
+export PROJECT_ID="platao-stark-hackathon"
+export APP=prophetstark 
+export PORT=8080
+export REGION="us-central1"
+export TAG="gcr.io/$PROJECT_ID/$APP"
+
+# Habilitar serviços necessários
+gcloud services enable cloudbuild.googleapis.com \
+    containerregistry.googleapis.com \
+    run.googleapis.com
+
+# Construir a imagem Docker
+docker build -t $TAG .
+
+# Enviar a imagem para o Google Cloud Build
+gcloud builds submit --tag $TAG
+
+# Deploy da aplicação no Google Cloud Run
+gcloud run deploy $APP --image $TAG --platform managed --region $REGION --allow-unauthenticated
+
+echo "Deploy concluído!"

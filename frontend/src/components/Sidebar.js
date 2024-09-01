17b74419cfb4c1c08f33441203b5b442b47e2d98
Leonardo Lins <leonardo.carvalho.8839@ga.ita.br>
2024-09-01 01:08:47 -0300

    Sidebar update

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,24 +1,129 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import AddIcon from "@mui/icons-material/Add";
 
-function Sidebar() {
-  return (
-    <div className="sidebar">
-      <nav>
-        <ul>
-          <li><Link to="/investimentos">Investimentos</Link></li>
-          <li><Link to="/recebimentos">Recebimentos</Link></li>
-          <li><Link to="/cartao-corporativo">Cartão Corporativo</Link></li>
-          <li><Link to="/integracoes">Integrações</Link></li>
-          <li><Link to="/consultas">Consultas</Link></li>
-        </ul>
-      </nav>
-      <div className="account-info">
-        <p>Hackathon Account 518467</p>
-        <p>hackathon-518467.sandbox.starkb...</p>
-      </div>
-    </div>
-  );
+export default function Sidebar() {
+    return (
+        <SidebarStyle>
+            <div id="sidebar">
+                <div id="menu-header">
+                    <img
+                        id="menu-logo"
+                        className="menu-icon"
+                        src="./images/logo.svg"
+                        alt="Logo"
+                    ></img>
+                </div>
+                <div id="menu-body">
+                    <span className="menu-text">Extrato</span>
+                    <span className="menu-text">Membros</span>
+                    <span className="menu-text">Crédito</span>
+                    <div className="menu-item">
+                        <ArrowRightIcon className="menu-icon" />
+                        <span>Investimentos</span>
+                    </div>
+                    <div className="menu-item">
+                        <ArrowRightIcon className="menu-icon" />
+                        <span>Recebimentos</span>
+                    </div>
+                    <div className="menu-item">
+                        <ArrowRightIcon className="menu-icon" />
+                        <span>Cartão Corporativo</span>
+                        <AddIcon className="add-icon" />
+                    </div>
+                    <div className="menu-item">
+                        <ArrowRightIcon className="menu-icon" />
+                        <span>Integrações</span>
+                    </div>
+                    <div className="menu-item">
+                        <ArrowRightIcon className="menu-icon" />
+                        <span>Consultas</span>
+                    </div>
+                    <div className="menu-item">
+                        <ArrowRightIcon className="menu-icon" />
+                        <span>Pagamentos</span>
+                        <AddIcon className="add-icon" />
+                    </div>
+                </div>
+                <div id="menu-footer">
+                    <p>Hackathon Account 518467</p>
+                    <p>hackathon-518467.sandbox.starkbank.com</p>
+                </div>
+            </div>
+        </SidebarStyle>
+    );
 }
 
-export default Sidebar;
+const SidebarStyle = styled.div`
+  #sidebar {
+    width: 240px;
+    height: 100%;
+    position: fixed;
+    z-index: 21;
+    background-color: #f7f9fa;
+    left: 0px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+
+  #menu-header {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  #menu-logo {
+    width: 40px;
+    height: 40px;
+  }
+
+  #menu-body {
+    display: flex;
+    flex-direction: column;
+    padding-left: 20px;
+    flex-grow: 1;
+  }
+
+  .menu-text {
+    margin-bottom: 15px;
+    margin-left: 35px;
+    font-size: 16px;
+    color: #4a4a4a;
+    cursor: pointer;
+  }
+
+  .menu-item {
+    display: flex;
+    align-items: center;
+    margin-bottom: 15px;
+    font-size: 16px;
+    color: #4a4a4a;
+    cursor: pointer;
+    position: relative;
+  }
+
+  .menu-icon {
+    margin-right: 10px;
+    color: #4a4a4a;
+  }
+
+  .add-icon {
+    position: absolute;
+    right: 0;
+    color: #4a4a4a;
+  }
+
+  #menu-footer {
+    padding-left: 20px;
+    padding-bottom: 20px;
+    font-size: 14px;
+    color: #4a4a4a;
+  }
+
+  #menu-footer p:last-child {
+    font-size: 12px;
+    color: #a0a0a0;
+  }
+`;
